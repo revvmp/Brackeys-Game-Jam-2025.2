65fb82a4e3c0f7218fae69e09d931168f544fa48
revamp <brandnguyen07@gmail.com>
2025-08-25 16:58:04 -0700

    FEAT: add timer to dice roll

--- mini_games/snake_eyes/dice.gd
+++ mini_games/snake_eyes/dice.gd
@@ -2,6 +2,11 @@ extends Node2D
 
 var num1 = 1
 var num2 = 1
+var animation_num1 = 1
+var animation_num2 = 1
+var time = 0.1
+
+
 signal snake_eyes
 signal dice_game_lost
 
@@ -9,13 +14,25 @@ signal dice_game_lost
 @onready var dice2 = $Dice2
 
 func roll():
+	for i in range (25):
+		await get_tree().create_timer(time).timeout
+		animation_num1 = randi_range(1, 6)
+		animation_num2 = randi_range(1, 6)
+		dice1.play(str(animation_num1))
+		dice2.play(str(animation_num2))
+		if i > 20:
+			time += 0.5
+		else:
+			time += 0.01
+	
 	num1 = randi_range(1, 6)
 	num2 = randi_range(1, 6)
 	dice1.play(str(num1))
 	dice2.play(str(num2))
+	time = 0.1
+	print("roll complete")
 	if num1 == num2:
 		emit_signal("snake_eyes")
 	else:
 		emit_signal("dice_game_lost")
 		
-	
