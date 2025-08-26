c37790c51dc1153a00a158f414ee76324dd3d59b
Bratah123 <brandonnguyen3301@gmail.com>
2025-08-25 22:13:19 -0700

    FEAT: Add third dice to bau cua ca cop

--- mini_games/bau_cua_cap_cop/bau_cua_ca_cop.gd
+++ mini_games/bau_cua_cap_cop/bau_cua_ca_cop.gd
@@ -1,6 +1,10 @@
 extends Node2D
 
 
-func _on_bau_cua_ca_cop_dice_dice_roll_result(dice_one_type: String, dice_two_type: String) -> void:
-	# Update Money Here
-	print("Dice One: " + str(dice_one_type) + " | Dice Two: " + str(dice_two_type))
+func _on_bau_cua_ca_cop_dice_dice_roll_result(dice_one_type: String, 
+											dice_two_type: String,
+											dice_three_type: String) -> void:
+	# Update Money Heres
+	print("Dice One: " + str(dice_one_type) + 
+			" | Dice Two: " + str(dice_two_type) +
+			" | Dice Three: " + str(dice_three_type))
--- mini_games/bau_cua_cap_cop/objects/bau_cua_ca_cop_dice.gd
+++ mini_games/bau_cua_cap_cop/objects/bau_cua_ca_cop_dice.gd
@@ -12,8 +12,11 @@ enum DiceType {
 
 @onready var dice_one = $DiceOne
 @onready var dice_two = $DiceTwo
+@onready var dice_three = $DiceThree
 
-signal dice_roll_result(dice_one_type: DiceType, dice_two_type: DiceType)
+signal dice_roll_result(dice_one_type: DiceType, 
+						dice_two_type: DiceType,
+						dice_two_three: DiceType)
 
 func _on_ready():
 	randomize()
@@ -22,13 +25,17 @@ func roll():
 	
 	dice_one.stop()
 	dice_two.stop()
+	dice_three.stop()
 	
 	var rand_result = randi_range(0, 5)
 	var rand_result_two = randi_range(0, 5)
+	var rand_result_three = randi_range(0, 5)
 	
 	dice_one.frame = rand_result
 	dice_two.frame = rand_result_two
+	dice_three.frame = rand_result_three
 	
 	dice_roll_result.emit(DiceType.keys()[rand_result],
-				DiceType.keys()[rand_result_two])
+						DiceType.keys()[rand_result_two],
+						DiceType.keys()[rand_result_three])
 	
