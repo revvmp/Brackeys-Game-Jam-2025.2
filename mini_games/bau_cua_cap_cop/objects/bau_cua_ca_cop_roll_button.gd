3dc5e1b502d270553b1c220c64b8924b028c0d4b
Bratah123 <brandonnguyen3301@gmail.com>
2025-08-25 18:13:52 -0700

    FEAT: Add fading text

--- mini_games/bau_cua_cap_cop/objects/bau_cua_ca_cop_roll_button.gd
+++ mini_games/bau_cua_cap_cop/objects/bau_cua_ca_cop_roll_button.gd
@@ -1,6 +1,8 @@
 extends "res://ui/button.gd"
 
 @onready var dice = $"../BauCuaCaCopDice"
+@onready var fading_text = $"../FadingText"
 
 func on_mouse_pressed():
+	fading_text.display_text("Invalid Money Amount", Color.RED)
 	dice.roll()
