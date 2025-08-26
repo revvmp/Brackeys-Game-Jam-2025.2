66ac5b8e31ca057fab91584b6a06ba5eec34fdee
Bratah123 <brandonnguyen3301@gmail.com>
2025-08-25 17:31:18 -0700

    REFACT: Move button outside of dice

--- mini_games/bau_cua_cap_cop/objects/bau_cua_ca_cop_roll_button.gd
+++ mini_games/bau_cua_cap_cop/objects/bau_cua_ca_cop_roll_button.gd
@@ -1,6 +1,7 @@
 extends "res://ui/button.gd"
 
-@onready var dice = $".."
+@onready var dice = $"../BauCuaCaCopDice"
+@onready var money_display = $"../MoneyDisplay"
 
 func on_mouse_pressed():
 	dice.roll()
