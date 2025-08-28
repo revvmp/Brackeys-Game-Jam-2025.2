9bb9c5110eb3060e884518d2f72d6619b7fc5424
Brian Nguyen <briannguyen2014094@gmail.com>
2025-08-27 23:40:16 -0700

    FEAT: added fading text to RPS

--- mini_games/rock_paper_scissors/objects/paper_button.gd
+++ mini_games/rock_paper_scissors/objects/paper_button.gd
@@ -3,8 +3,10 @@ extends "res://ui/button.gd"
 signal paper_button
 
 @onready var paper = $"../RockPaperScissorsOptions"
+@onready var fading_text = $"../FadingText"
 
 func on_mouse_pressed():
 		paper.make_rps_choice()
 		emit_signal("paper_button")
 		print("paper")
+		fading_text.display_text("Invalid Money Amount", Color.RED)
--- mini_games/rock_paper_scissors/objects/scissors_button.gd
+++ mini_games/rock_paper_scissors/objects/scissors_button.gd
@@ -3,8 +3,10 @@ extends "res://ui/button.gd"
 signal scissors_button
 
 @onready var scissors = $"../RockPaperScissorsOptions"
+@onready var fading_text = $"../FadingText"
 
 func on_mouse_pressed():
 		scissors.make_rps_choice()
 		emit_signal("scissors_button")
 		print("scissors")
+		fading_text.display_text("Invalid Money Amount", Color.RED)
