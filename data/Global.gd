4cc517a97d8fccb69c1d57d1ded07fa8016e28f4
Bratah123 <brandonnguyen3301@gmail.com>
2025-08-27 23:49:20 -0700

    FEAT: Add game database to save money and inventory

--- data/Global.gd
+++ data/Global.gd
@@ -1,6 +1,101 @@
 extends Node
 
+# ORDRED BY PRICE
+enum Trophy {
+	# TROPHY_NAME = (ID, PRICE)
+	DONUT = 0,
+	COOKIE = 1,
+	CHEESECAKE = 2,
+	CINNAMON_ROLL = 3,
+	CAKE = 4,
+	BISCUITS = 5,
+}
+
+enum Price {
+	# TROPHY_NAME = (ID, PRICE)
+	DONUT = 1_000,
+	COOKIE = 5_000,
+	CHEESECAKE = 15_000,
+	CINNAMON_ROLL = 30_000,
+	CAKE = 50_000,
+	BISCUITS = 100_000,
+}
+
 var money: int = 100
+var trophies: Array = []
+var shop: Array = []
+
+const SAVE_FILE_NAME = "user://devilsdeal.save"
+
+func _ready() -> void:
+	wipe_game()
+	if not FileAccess.file_exists(SAVE_FILE_NAME):
+		money = 100
+		trophies = []
+		shop = [
+			Trophy.DONUT,
+			Trophy.COOKIE,
+			Trophy.CHEESECAKE,
+			Trophy.CINNAMON_ROLL,
+			Trophy.CAKE,
+			Trophy.BISCUITS,
+		]
+		print("INSTANTIED NEW SAVE - TROPHIES: ", trophies, " | SHOP: ", shop, " | Money: ", money)
+		save_global_data()
+		return # Fresh Game, instantiate objects
+	load_game()
+	print("Successfully loaded game data")
+	print(trophies)
+	print(shop)
 
 func set_money(new_money: int):
 	money = new_money
+	
+func add_trophy(trophy: Trophy):
+	trophies.append(trophy)
+
+func _save_json():
+	var save_dict = {
+		"filename": get_scene_file_path(),
+		"money": money,
+		"trophies": trophies,
+		"shop": shop,
+	}
+	
+	return save_dict
+
+func save_global_data():
+	var save_file = FileAccess.open(SAVE_FILE_NAME, FileAccess.WRITE)
+	save_file.store_line(
+		JSON.stringify(
+			_save_json()
+		)
+	)
+	
+func load_game():
+	# Load the file line by line and process that dictionary to restore
+	# the object it represents.
+	var save_file = FileAccess.open(SAVE_FILE_NAME, FileAccess.READ)
+	while save_file.get_position() < save_file.get_length():
+		var json_string = save_file.get_line()
+
+		# Creates the helper class to interact with JSON.
+		var json = JSON.new()
+
+		# Check if there is any error while parsing the JSON string, skip in case of failure.
+		var parse_result = json.parse(json_string)
+		if not parse_result == OK:
+			print("JSON Parse Error: ", json.get_error_message(), " in ", json_string, " at line ", json.get_error_line())
+			continue
+
+		# Get the data from the JSON object.
+		var node_data = json.data
+
+		# Now we set the remaining variables.
+		money = node_data["money"]
+		trophies = node_data["trophies"]
+		shop = node_data["shop"]
+
+func wipe_game():
+	DirAccess.remove_absolute(SAVE_FILE_NAME)
+	
