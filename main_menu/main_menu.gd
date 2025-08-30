a079a008beb9741eefe2cd584e683b402fa0563a
Bratah123 <brandonnguyen3301@gmail.com>
2025-08-29 23:29:22 -0700

    FEAT: Add link from play button to random games

--- main_menu/main_menu.gd
+++ main_menu/main_menu.gd
@@ -1,7 +1,18 @@
 extends Node2D
 
+const MINIGAMES = [
+	"res://mini_games/bau_cua_cap_cop/BauCuaCaCop.tscn",
+	"res://mini_games/rock_paper_scissors/RockPaperScissors.tscn",
+	"res://mini_games/snake_eyes/DiceGame.tscn",
+	"res://mini_games/wheel_of_fortune/WheelOfFortune.tscn",
+]
+
 func _on_help_button_help_pressed() -> void:
 	get_tree().change_scene_to_file("res://main_menu/help_menu/HelpMenu.tscn")
 
 func _on_shop_button_on_shop_pressed() -> void:
 	get_tree().change_scene_to_file("res://main_menu/shop_menu/ShopMenu.tscn")
+
+func _on_play_button_on_play_pressed() -> void:
+	var rand_int = randi_range(0, MINIGAMES.size()-1)
+	print("LOAD ", MINIGAMES[rand_int])
