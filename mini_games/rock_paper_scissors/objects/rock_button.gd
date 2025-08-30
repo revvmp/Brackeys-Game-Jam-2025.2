28c66454732f1402c313f48cee6de9aa79c6b92e
Brian Nguyen <briannguyen2014094@gmail.com>
2025-08-30 01:24:29 -0700

    FEAT: Add animation to RPS

--- mini_games/rock_paper_scissors/objects/rock_button.gd
+++ mini_games/rock_paper_scissors/objects/rock_button.gd
@@ -7,6 +7,6 @@ var pressable = false
 @onready var fading_text = $"../FadingText"
 
 func on_mouse_pressed(): 
-		if pressable and Global.money > 0:
-			emit_signal("rock_button")
-			print("rock")
+	if pressable and Global.money > 0:
+		emit_signal("rock_button")
+		print("rock")
