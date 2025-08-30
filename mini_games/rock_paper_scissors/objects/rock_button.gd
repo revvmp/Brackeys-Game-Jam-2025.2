dd3688e44cec3abaf771d98ca445e22a8824c771
Brian Nguyen <briannguyen2014094@gmail.com>
2025-08-29 18:31:10 -0700

    FEAT: add money input

--- mini_games/rock_paper_scissors/objects/rock_button.gd
+++ mini_games/rock_paper_scissors/objects/rock_button.gd
@@ -1,10 +1,12 @@
 extends "res://ui/button.gd"
 
 signal rock_button
+var pressable = false
 
 @onready var rock = $"../RockPaperScissorsOptions"
 @onready var fading_text = $"../FadingText"
 
-func on_mouse_pressed():
-		emit_signal("rock_button")
-		print("rock")
+func on_mouse_pressed(): 
+		if pressable:
+			emit_signal("rock_button")
+			print("rock")
--- mini_games/rock_paper_scissors/objects/scissors_button.gd
+++ mini_games/rock_paper_scissors/objects/scissors_button.gd
@@ -1,10 +1,12 @@
 extends "res://ui/button.gd"
 
 signal scissors_button
+var pressable = false
 
 @onready var scissors = $"../RockPaperScissorsOptions"
 @onready var fading_text = $"../FadingText"
 
 func on_mouse_pressed():
+	if pressable:
 		emit_signal("scissors_button")
 		print("scissors")
