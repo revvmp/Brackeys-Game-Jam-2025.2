d2ae503a2a2d1d9007ea6e5ffbef21a66126dc35
revamp <brandnguyen07@gmail.com>
2025-08-30 23:16:55 -0700

    FEAT: Add result for each dice_type in BCCC

--- mini_games/bau_cua_cap_cop/bau_cua_ca_cop.gd
+++ mini_games/bau_cua_cap_cop/bau_cua_ca_cop.gd
@@ -4,6 +4,7 @@ extends Node2D
 @onready var button = $RollButton
 @onready var money = $MoneyDisplay
 @onready var text = $FadingText
+@onready var all_text = $AllFadingText
 
 func _on_bau_cua_ca_cop_dice_dice_roll_result(dice_one_type: String, 
 											dice_two_type: String,
@@ -13,23 +14,31 @@ func _on_bau_cua_ca_cop_dice_dice_roll_result(dice_one_type: String,
 			" | Dice Two: " + str(dice_two_type) +
 			" | Dice Three: " + str(dice_three_type))
 
-	var bets: String
+	var bets = []
 	for key in button.amounts.keys():
 		var value = button.amounts[key]
 		if value > 0:
-			bets += key
+			bets.append(key)
 			print(key + " has a bet of " + str(value))
 	print(bets)
 	
-	var amount_won: int
-	if dice_one_type in bets:
-		money.add_money(button.amounts[dice_one_type] * 2)
-		amount_won += button.amounts[dice_one_type] * 2
-	if dice_two_type in bets:
-		money.add_money(button.amounts[dice_two_type] * 2)
-		amount_won += button.amounts[dice_two_type] * 2
-	if dice_three_type in bets:
-		money.add_money(button.amounts[dice_three_type] * 2)
-		amount_won += button.amounts[dice_three_type] * 2
+	var dice_result = [dice_one_type, dice_two_type, dice_three_type]
+	var amount_won = 0
+	
+	for type in bets:
+		var count = dice_result.count(type)
+		var pay = 0
+		if count > 0:
+			pay = button.amounts[type] * (count + 1)
+			money.add_money(pay)
+			amount_won += pay
+		
+		var result = all_text.get_node(type)
+		if pay > 0:
+			result.display_text("+" + str(pay), Color.GREEN)
+		else:
+			result.display_text("-" + str(button.amounts[type]), Color.RED)
+	
 	text.display_text("You bet $" + str(button.total) + " and won $" + str(amount_won), Color.GREEN)
+	TransitionScene.transition_to_main_menu()
 		
