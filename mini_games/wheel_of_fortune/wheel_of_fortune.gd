f35c3d31bc4724886cdb22244156ca87e95b0552
Bratah123 <brandonnguyen3301@gmail.com>
2025-08-30 22:52:58 -0700

    FEAT: Add transition back to menu for wheel of fortune

--- mini_games/wheel_of_fortune/wheel_of_fortune.gd
+++ mini_games/wheel_of_fortune/wheel_of_fortune.gd
@@ -13,17 +13,19 @@ func _on_button_spin_wheel() -> void:
 func _on_wheel_lose_money() -> void:
 	amount.get_child(0).editable = true
 	text.display_text("You lost.", Color.RED)
+	TransitionScene.transition_to_main_menu()
 
 
 func _on_wheel_win_3x() -> void:
 	amount.get_child(0).editable = true
 	money.add_money(amount.value * 4)
-	text.display_text("You 3x your money!", Color.SKY_BLUE)
-	
+	text.display_text("You 3x your money!", Color.GREEN)
+	TransitionScene.transition_to_main_menu()
 
 
 func _on_wheel_win_5x() -> void:
 	amount.get_child(0).editable = true
 	money.add_money(amount.value * 6)
-	text.display_text("You 5x your money!!", Color.SKY_BLUE)
+	text.display_text("You 5x your money!!", Color.GREEN)
+	TransitionScene.transition_to_main_menu()
 	
